a9356bb680e00bd733e0aca7d061aa3adce3f9fd
mchau5 <59899035+mchau5@users.noreply.github.com>
2020-03-06 14:54:24 -0600

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
-import InputHandler from "input.js";
-import Player from "player.js";
-import Harm from "harm.js";
-import Coin from "coin.js";
+import InputHandler from "./input.js";
+import Player from "./player.js";
+import Harm from "./harm.js";
+import Coin from "./coin.js";
 
 
 
